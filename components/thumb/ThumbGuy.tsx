d69a65b80ffd9b843961dd2357721e507288cf2b
jan-czerwinski <jan.ignacy.czerwinski@gmail.com>
2022-02-12 12:12:20 +0100

    optimize thumb guy loading

--- components/thumb/ThumbGuy.tsx
+++ components/thumb/ThumbGuy.tsx
@@ -1,33 +1,34 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef, useMemo } from 'react';
 import { useFrame } from '@react-three/fiber';
 import { Html } from '@react-three/drei';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { Object3D } from 'three/src/core/Object3D'; //Object3D types
-import { Vector3 } from 'three';
+import { Euler, Vector3 } from 'three';
 
 //This model loading is based on Model.tsx file from this example:
 //https://github.dev/oslavdev/next-three-example
 
-export type ThumbGuyProps = {
+export type LocationType = {
   position: Vector3;
   scale: Vector3;
+  rotation: Euler;
+};
+export type ThumbGuyProps = LocationType & {
+  model: Object3D;
 };
 
 const ThumbGuy = (props: ThumbGuyProps) => {
-  const [model, setModel] = useState<Object3D | null>(null);
+  //this is cloning the model passed as a prop for efficiency reasons
+  const clonedModel = useMemo(() => props.model?.clone(), [props.model]);
 
-  /* Load model */
-  useEffect(() => {
-    const loader = new GLTFLoader();
-    loader.load('/three/thumb_guy/scene.gltf', async (gltf) => {
-      const nodes = await gltf.parser.getDependencies('node');
-      setModel(nodes[0]);
-    });
-  }, []);
-
-  return model ? (
-    <group position={props.position} dispose={null} scale={props.scale}>
-      <primitive name="Object_0" object={model} />
+  return clonedModel ? (
+    <group
+      position={props.position}
+      rotation={props.rotation}
+      scale={props.scale}
+      dispose={null}
+    >
+      <primitive name="Object_0" object={clonedModel} />
     </group>
   ) : (
     <Html>Loading...</Html>
--- pages/thumb.tsx
+++ pages/thumb.tsx
@@ -1,11 +1,12 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { Matrix4, Vector3 } from 'three';
+import { Euler, Matrix4, Object3D, Vector3 } from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 import CameraControls from '../components/CameraControls';
-import ThumbGuy, { ThumbGuyProps } from '../components/thumb/ThumbGuy';
+import ThumbGuy, { LocationType } from '../components/thumb/ThumbGuy';
 
-// const ThumbLeftArm: Matrix4 = {
+// const ThumbLeftArm: LocationType = {
 //TODO Ta funkcja ma generować rotację dla lewej ręki -
 //TODO analogicznie ma być dla prawej, nóg i głowy
 //TODO rotacje całości, tak zeby mozna było przyczepić i zrobić fraktal
@@ -13,14 +14,17 @@ import ThumbGuy, { ThumbGuyProps } from '../components/thumb/ThumbGuy';
 
 const generateThumbLocations = (): AllThumbLocationsType => {
   const thumbLocations: AllThumbLocationsType = [];
-  const cubeSize = 1;
-  for (let x = -cubeSize; x < cubeSize; x++) {
-    for (let y = -cubeSize; y < cubeSize; y++) {
-      for (let z = -cubeSize; z < cubeSize; z++) {
-        const scale = (y % 2) + 1; //swap between 2 values (1,2)
+  const cubeSize = 2;
+
+  const scale = 1;
+  for (let x = -cubeSize; x <= cubeSize; x++) {
+    for (let y = -cubeSize; y <= cubeSize; y++) {
+      for (let z = -cubeSize; z <= cubeSize; z++) {
+        const scale = 0.3;
         thumbLocations.push({
-          position: new Vector3(x * 3, y * 3, z * 3),
+          position: new Vector3(x, y, z),
           scale: new Vector3(scale, scale, scale),
+          rotation: new Euler(0, Math.PI, 0),
         });
       }
     }
@@ -29,18 +33,38 @@ const generateThumbLocations = (): AllThumbLocationsType => {
   return thumbLocations;
 };
 
-type AllThumbLocationsType = ThumbGuyProps[];
+type AllThumbLocationsType = LocationType[];
+
 const Thumb = () => {
   const [thumbLocations, setThumbLocations] = useState<AllThumbLocationsType>(
     generateThumbLocations()
   );
 
+  const [thumbModel, setThumbModel] = useState<Object3D>(null!);
+  useMemo(() => {
+    // this loads the thumb guy model
+    //This model loading is based on Model.tsx file from this example:
+    //https://github.dev/oslavdev/next-three-example
+    const loader = new GLTFLoader();
+    let nodes: Object3D[];
+    loader.load('/three/thumb_guy/scene.gltf', async (gltf) => {
+      nodes = await gltf.parser.getDependencies('node');
+      setThumbModel(nodes[0]);
+    });
+  }, []);
+
   return (
     <div className="w-screen h-screen bg-yellow-400">
       <Canvas>
         <CameraControls />
-        {thumbLocations.map(({ position, scale }, idx) => (
-          <ThumbGuy position={position} scale={scale} key={idx} />
+        {thumbLocations.map(({ position, scale, rotation }, idx) => (
+          <ThumbGuy
+            key={idx}
+            position={position}
+            scale={scale}
+            rotation={rotation}
+            model={thumbModel}
+          />
         ))}
 
         <ambientLight />
